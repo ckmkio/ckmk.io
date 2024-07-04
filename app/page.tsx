7e6d028cfddc26a4656c25e952ff536541a173c0
Kelvin Cheong <ckmkio@icloud.com>
2024-07-03 23:40:01 -0400

    Update nav bar and add job timeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,42 @@
-import {Avatar, Blockquote, HR} from "flowbite-react";
-import Image from "next/image";
+import {Avatar} from "flowbite-react";
 import LogoCarousel from "@/app/components/languagecarousel";
 import FrameworkCarousel from "@/app/components/frameworkcarousel";
+import JobTimeline from "@/app/components/jobtimeline";
 
 export default function Home() {
   return (
       <div className="flex flex-wrap">
-          <div className="mt-20 flex space-x-12">
-              <div className="text-white grow">
-                  <div className="text-white text-[48px] font-semibold uppercase">Kelvin Cheong</div>
-                  <div className="text-white text-[48px] font-semibold uppercase">Senior Software Enginner
-                  </div>
-                  <div className="text-white text-2xl font-normal capitalize">
-                      8 years in full stack and mobile development, including significant roles at Google and Fitbit.
-                      Proven track record in driving successful product launches and experience in major acquisitions:
-                      Doki {'>'} Fitbit {'>'} Google.
-                  </div>
-              </div>
-              <Avatar
-                  className="flex-none w-[30%] h-[30%] "
-                  placeholderInitials="KC"
-                  alt="avatar of Kelvin"
-                  size="xl"
-                  rounded/>
+
+          <Avatar
+              className="mt-16 flex-none"
+              placeholderInitials="KC"
+              alt="avatar of Kelvin"
+              size="xl"
+              rounded>
+
+              <div className="text-[32px] font-semibold uppercase text-gray-700">Kelvin Cheong</div>
+              <div className="text-[48px] font-semibold uppercase text-gray-700">Senior Software Enginner</div>
+          </Avatar>
+
+          <div className="mt-8 text-xl font-normal text-gray-700">
+              8 years in full stack and mobile development, including significant roles at Google and Fitbit.
+              Proven track record in driving successful product launches and experience in major acquisitions:
+              Doki {'>'} Fitbit {'>'} Google.
           </div>
 
-          <div className='mt-20'>
+          <div className="mt-16">
+              <JobTimeline/>
+          </div>
+
+          <div
+              className="mt-16 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_96px,_black_calc(100%-96px),transparent_100%)]">
               <LogoCarousel/>
+          </div>
+
+          <div
+              className="mt-8 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_96px,_black_calc(100%-96px),transparent_100%)]">
               <FrameworkCarousel/>
           </div>
-
       </div>
   );
 }
