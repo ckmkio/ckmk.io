c67b8a8ff005fe58e6a4d1ec02395ded61fb6959
ckmkio <148254741+ckmkio@users.noreply.github.com>
2024-07-04 00:08:54 -0400

    Merge to main branch (#2)
    * Update navbar styles
    
    * Update tech stack section
    
    * Update nav bar and add job timeline
    
    * Update career timeline

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,12 +5,12 @@ export default function MyNavbar() {
     return (
         <Navbar className="bg-slate-800">
             <NavbarBrand>
-                <span className="whitespace-nowrap text-xl text-slate-300">Kelvin Cheong&#39;s Portfolio</span>
+                <span className="whitespace-nowrap text-xl text-gray-300">ckmk.io</span>
             </NavbarBrand>
             <NavbarToggle className="mr-3 h-6 sm:h-9" />
             <NavbarCollapse>
-                <NavbarLink className="text-slate-300" as={Link} href="/home">About me</NavbarLink>
-                <NavbarLink className="text-slate-300" as={Link} href="/projects">Projects</NavbarLink>
+                <NavbarLink className="text-gray-300" as={Link} href="/">About me</NavbarLink>
+                <NavbarLink className="text-gray-300" as={Link} href="/projects">Projects</NavbarLink>
             </NavbarCollapse>
         </Navbar>
     );
